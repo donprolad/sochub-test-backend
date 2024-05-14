34670202f93896ca0fabb64d0b99247fe3b706a1
Don <dghdp37@gmail.com>
2024-05-14 12:06:18 +0200

    feature/guards - route guards and validations for reset password.

--- src/middleware/guards/schemas.js
+++ src/middleware/guards/schemas.js
@@ -25,3 +25,16 @@ export const registrationSchema = Joi.object({
   }),
   organization: Joi.string().required(),
 });
+
+export const resetPasswordSchema = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .pattern(new RegExp("^[a-z0-9@.]"))
+    .required(),
+  password: Joi.string()
+    .pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{12,}$/)
+    .required(),
+  confirm_password: Joi.ref("password"),
+})
+  .xor("password", "access_token")
+  .with("password", "confirm_password");
