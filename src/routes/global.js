d239f5ba4181195c5ea891373bce351174833a8c
Don <dghdp37@gmail.com>
2024-05-14 12:09:53 +0200

    feature/guards - merge conflicts.

--- src/routes/global.js
+++ src/routes/global.js
@@ -7,11 +7,6 @@ import {
   resetPassword,
 } from "../controllers/global.js";
 
-<<<<<<< Updated upstream
-import { passwordSchema, registrationSchema } from '../middleware/guards/schemas.js'
-import { guard } from '../middleware/guards/helper.js'
-
-=======
 import {
   passwordSchema,
   registrationSchema,
@@ -19,21 +14,15 @@ import {
   resetPasswordSchema
 } from "../middleware/guards/schemas.js";
 import { guard } from "../middleware/guards/helper.js";
->>>>>>> Stashed changes
 
 const systemRouter = express.Router();
 
 // systemRouter.route("/token").post(getToken); // This will be removed, once login works
 systemRouter.route("/register").post(guard(registrationSchema),register);
 systemRouter.route("/login").post(guard(passwordSchema), login, getToken);
-<<<<<<< Updated upstream
-systemRouter.route("/forgot_password").post(forgotPassword);
-systemRouter.route("/reset_password").post(resetPassword);
-=======
 systemRouter
   .route("/forgot_password")
   .post(guard(forgottenPasswordSchema), forgotPassword);
 systemRouter.route("/reset_password").post(guard(resetPasswordSchema), resetPassword);
->>>>>>> Stashed changes
 
 export default systemRouter;
